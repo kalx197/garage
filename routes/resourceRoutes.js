4db0373a4239f09499981acd7d94fa0959425a4b
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-08 09:42:06 -0600

    Replace resource routes with tool routes

--- routes/resourceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const resourceController = require('../controllers/resourceController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware, resourceController.createResource);
-router.get('/', authMiddleware, resourceController.getUserResources);
-
-module.exports = router;
