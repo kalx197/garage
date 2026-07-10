8b1740922c7d0fba297cfc8210099a0355e637a5
kalx197 <kaleabteklemariam197@gmail.com>
2026-07-10 08:44:57 -0600

    Delete routes directory

--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-const { verifyToken } = require('../middleware/authMiddleware');
-
-router.post('/', verifyToken, orderController.placeOrder);
-router.get('/', verifyToken, orderController.getMyOrders);
-
-module.exports = router;
--- routes/toolRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const toolController = require('../controllers/toolController');
-const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
-
-router.get('/', toolController.getAllTools); // Anyone can see the catalog
-router.post('/', verifyToken, isAdmin, toolController.addTool); // Only Admins can add tools
-
-module.exports = router;
